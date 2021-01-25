20cd41d8a913602dfaecac5b3cfd86859e8054d6
pramodh <77781010+pramodh42@users.noreply.github.com>
2021-01-25 13:44:04 +0530

    Create choose.c

---
+++ choose.c
@@ -0,0 +1,22 @@
+{
+    int choice;
+    printf("enter your choice\n so that i will choose an item for you to eat\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1 : printf("have a pizza , cost is239");
+        break;
+        case 2: printf("have a burger , cost is 129 ");
+        break;
+        case 3 : printf("have a pasta, cost is 179");
+        break;
+        case 4 : printf("have a frenchfries , cost is 99");
+        break;
+        case 5 : printf("have a sandwitch, cost is 149");
+        break;
+        default:printf("invalid choice");
+        
+    }
+    return 0;
+    
+}
